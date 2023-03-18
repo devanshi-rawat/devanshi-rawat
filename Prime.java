f61ec3e1d8767fd73a185dc307d420b20c27b915
Devanshi Rawat <116871160+devanshi-rawat@users.noreply.github.com>
2023-03-18 19:17:46 +0530

    Add files via upload
    Assignment

---
+++ Prime.java
@@ -0,0 +1,24 @@
+class Prime
+{
+    public static void main(String[] args)
+    {
+        int n=0,i=1,j=1,ct=0;;
+        while(n<25)
+        {
+            j=1;
+            ct=0;
+            while(j<=i)
+            {
+                if(i%j==0)
+                    ct++;
+                j++;
+            }
+            if(ct==2)
+            {
+                System.out.printf("%d ",i);
+                n++;
+            }
+            i++;
+        }
+    }
+}
